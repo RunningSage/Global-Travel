4ef824ab4cde1bf2f5cc1f97c7eabe01dbadfb2a
RunningSage <132100509+RunningSage@users.noreply.github.com>
2024-02-15 00:06:39 +0530

    fixed slick bugs

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button1 from '../Components/Button1'
 function About() {
-  return (
+  return ( 
     <>
               <div id="about" className="about">
           <div className="container">
